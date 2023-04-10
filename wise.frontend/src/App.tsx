0689bf996b12f2efbb03af10b757739be85b1ce6
henriqferreira <henriquepfn@gmail.com>
2023-04-10 11:44:35 -0300

    Fix: Fixing layout rendering

--- wise.frontend/src/App.tsx
+++ wise.frontend/src/App.tsx
@@ -1,24 +1,15 @@
 import { Component } from 'react';
 import Sidebar from './layouts/_Sidebar/_Sidebar';
 import "./assets/css/global.css";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import Assignments from './pages/Assignments/Assignments';
-import Rooms from './pages/Rooms/Rooms';
-import Pages from './pages/Pages/Pages';
+import { Outlet } from 'react-router-dom';
 
 export default class App extends Component {
     render () {
         return (
-            <BrowserRouter>
+            <>
                 <Sidebar />
-                <Routes>
-                    <Route path='/' element={ <Home /> } />
-                    <Route path='/assignments' element={ <Assignments /> } />
-                    <Route path='/pages' element={ <Pages /> } />
-                    <Route path='/rooms' element={ <Rooms /> } />
-                </Routes>
-            </BrowserRouter>
+                <Outlet />
+            </>
         );
     }
 }
--- wise.frontend/src/index.tsx
+++ wise.frontend/src/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Home from './pages/Home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Assignments from './pages/Assignments/Assignments';
+import Pages from './pages/Pages/Pages';
+import Rooms from './pages/Rooms/Rooms';
+import Login from './pages/Login/Login';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
@@ -8,6 +14,16 @@ const root = ReactDOM.createRoot(
 
 root.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={ <App /> }>
+                    <Route index element={ <Home /> } />
+                    <Route path='assignments' element={ <Assignments /> } />
+                    <Route path='pages' element={ <Pages /> } />
+                    <Route path='rooms' element={ <Rooms /> } />
+                </Route>
+                <Route path='/login' element={ <Login /> } />
+            </Routes>
+        </BrowserRouter>
     </React.StrictMode>
 );
