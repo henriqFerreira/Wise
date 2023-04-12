db45363506759f26cdae00b74ba0a3a5acf25b3c
henriqferreira <henriquepfn@gmail.com>
2023-04-12 07:36:57 -0300

    Fix: Fixing layouts css

--- wise.frontend/src/App.tsx
+++ wise.frontend/src/App.tsx
@@ -1,15 +1,16 @@
 import { Component } from 'react';
-import Sidebar from './layouts/_Sidebar/_Sidebar';
-import "./assets/css/App.css";
 import { Outlet } from 'react-router-dom';
+import Sidebar from './layouts/_Sidebar/_Sidebar';
+import styles from "./assets/css/App.module.css";
+import "./assets/css/global.css";
 
 export default class App extends Component {
     render () {
         return (
-            <>
+            <div className={ styles.content }>
                 <Sidebar />
                 <Outlet />
-            </>
+            </div>
         );
     }
 }
--- wise.frontend/src/AuthApp.tsx
+++ wise.frontend/src/AuthApp.tsx
@@ -1,15 +1,16 @@
 import { Component } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./layouts/_Header/_Header";
-import "./assets/css/AuthApp.css";
+import styles from "./assets/css/AuthApp.module.css";
+import "./assets/css/global.css";
 
 export default class AuthApp extends Component {
     render () {
         return (
-            <>
+            <div className={ styles.content }>
                 <Header />
                 <Outlet />
-            </>
+            </div>
         );
     }
 }
